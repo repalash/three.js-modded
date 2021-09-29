80bd3d937a1efd77771b52f482f36a00ed063c4d
Palash Bansal <palashbansal96@gmail.com>
2021-09-29 21:37:12 +0530

    opaque and transparent render flags

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1266,9 +1266,14 @@ function WebGLRenderer( parameters = {} ) {
 
 		if ( viewport ) state.viewport( _currentViewport.copy( viewport ) );
 
-		if ( ! _this.userData.transmissionRender ) {
+		if ( _this.userData.opaqueRender !== false ) {
 
 			if ( opaqueObjects.length > 0 ) renderObjects( opaqueObjects, scene, camera );
+
+		}
+
+		if ( _this.userData.transparentRender !== false ) {
+
 			if ( transparentObjects.length > 0 ) renderObjects( transparentObjects, scene, camera );
 
 		}
