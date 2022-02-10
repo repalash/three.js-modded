77ba57d7fabed3aa53e75f84b81df50ea7df43e4
Palash Bansal <palashbansal96@gmail.com>
2022-02-10 22:48:24 +0530

    Object loader image complete fix.

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -432,7 +432,8 @@ class ObjectLoader extends Loader {
 					return {
 						data: getTypedArray( image.type, image.data ),
 						width: image.width,
-						height: image.height
+						height: image.height,
+						complete: true
 					};
 
 				} else {
