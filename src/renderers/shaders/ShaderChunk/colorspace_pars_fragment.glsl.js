733c45c4fe3c1f1a30fe6a39247df2c0731b4162
Palash Bansal <palashbansal96@gmail.com>
2025-02-08 15:11:33 +0530

    Remove RGBMToLinear, LinearToRGBM

--- src/renderers/shaders/ShaderChunk/colorspace_pars_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/colorspace_pars_fragment.glsl.js
@@ -47,20 +47,6 @@ vec4 sRGBToLinear( in vec4 value ) {
 	return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
 }
 
-
-// reference: http://iwasbeingirony.blogspot.ca/2010/06/difference-between-rgbm-and-rgbd.html
-vec4 RGBMToLinear( in vec4 value, in float maxRange ) {
-	return vec4( value.rgb * value.a * maxRange, 1.0 );
-}
-
-vec4 LinearToRGBM( in vec4 value, in float maxRange ) {
-	float maxRGB = max( value.r, max( value.g, value.b ) );
-	float M = clamp( maxRGB / maxRange, 0.0, 1.0 );
-	M = ceil( M * 255.0 ) / 255.0;
-	return vec4( value.rgb / ( M * maxRange ), M );
-}
-
-
 // reference: http://iwasbeingirony.blogspot.ca/2010/06/difference-between-rgbm-and-rgbd.html
 vec4 RGBM16ToLinear( in vec4 value ) {
 	return vec4( value.rgb * value.a * 16.0, 1.0 );
