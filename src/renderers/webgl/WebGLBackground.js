0889b34788b4011b998a3bb7b71f35674b35130d
Palash Bansal <palashbansal96@gmail.com>
2025-10-24 11:53:55 +0530

    Deprecated warn fix

--- src/renderers/webgl/WebGLBackground.js
+++ src/renderers/webgl/WebGLBackground.js
@@ -139,7 +139,7 @@ function WebGLBackground( renderer, cubemaps, cubeuvmaps, state, objects, alpha,
 
 			if ( isEnvironment ) {
 
-				boxMesh.material.uniforms.backgroundIntensity.value *= scene.envMapIntensity; // special for Scene in webgi
+				boxMesh.material.uniforms.backgroundIntensity.value *= scene.environmentIntensity;
 
 				_e1.copy( scene.environmentRotation );
 
